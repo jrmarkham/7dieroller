e65300b7d938be075ed330b8611895f93bad25c5
Felix Angelov <felangelov@gmail.com>
2019-10-06 15:07:39 -0400

    Update to Equatable v0.6.0

--- lib/src/data/blocs/firebase/firebase_state.dart
+++ lib/src/data/blocs/firebase/firebase_state.dart
@@ -1,15 +1,28 @@
 import 'package:equatable/equatable.dart';
 
 abstract class FirebaseState extends Equatable {
-  FirebaseState([List props = const []]):super(props);
+  const FirebaseState();
 }
 
-class FirebaseStateInit extends FirebaseState {}
-class FirebaseStateLoading extends FirebaseState {}
+class FirebaseStateInit extends FirebaseState {
+  @override
+  List<Object> get props => [];
+}
+
+class FirebaseStateLoading extends FirebaseState {
+  @override
+  List<Object> get props => [];
+}
+
 class FirebaseStateInitResponse extends FirebaseState {
   final Stream stream;
-  FirebaseStateInitResponse(this.stream):super([stream]);
-}
-class FirebaseStateLoaded extends FirebaseState {
+  const FirebaseStateInitResponse(this.stream);
 
+  @override
+  List<Object> get props => [stream];
+}
+
+class FirebaseStateLoaded extends FirebaseState {
+  @override
+  List<Object> get props => [];
 }
