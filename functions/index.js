dd94308bc3676b4fa6c57a3d12308b92336a956d
me@markhamenterprises.com <me@markhamenterprises.com>
2019-10-07 15:17:49 -0400

    add firestore function --> auto update to unlock when sessions are interupted

--- functions/index.js
+++ functions/index.js
@@ -6,20 +6,25 @@ const functions = require('firebase-functions');
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
-// The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
-const functions = require('firebase-functions');
+const TIME = 15 * 1000;
+// function delayUpdate(ms, func){
+//     return new Promise(resolve=>{
+//         return setTimeout(func,ms)
+//     })
+// }
 
-// The Firebase Admin SDK to access the Firebase Realtime Database.
-const admin = require('firebase-admin');
-admin.initializeApp();
+exports.unlockAutoUpdate = functions.firestore
+    .document('dieroller/main').onUpdate((change, context) => {
+        const isLocked = change.after.data().locked;
+        const update = ()=> change.after.ref.update({'locked':false});
 
-// Take the text parameter passed to this HTTP endpoint and insert it into the
-// Realtime Database under the path /messages/:pushId/original
-exports.addMessage = functions.https.onRequest(async (req, res) => {
-    // Grab the text parameter.
-    const original = req.query.text;
-// Push the new message into the Realtime Database using the Firebase Admin SDK.
-const snapshot = await admin.database().ref('/messages').push({original: original});
-// Redirect with 303 SEE OTHER to the URL of the pushed object in the Firebase console.
-res.redirect(303, snapshot.ref.toString());
-});
+        const timeUpdate =  setTimeout(update, TIME);
+        console.log('isLocked ', isLocked);
+        if(!isLocked){
+            console.log('unref');
+            return timeUpdate.unref();
+        }
+
+        console.log('running delay update');
+        return new Promise(resolve => timeUpdate.ref());
+    });
